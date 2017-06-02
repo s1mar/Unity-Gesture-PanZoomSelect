6483fdb62b82d7947ab68f962f48299c18209170
s1mar <simarjot@aol.in>
2017-06-02 11:36:18 +0530

    firstCommit

---
+++ Scripts/GestureScript.cs
@@ -0,0 +1,228 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureScript : MonoBehaviour {
+
+	GameObject selectedGameObject;
+	public float PAN_SPEED = 0.3f;
+	float touchTime;
+	public float SCALE_DURATION = 2.0f;
+	bool rotationWidgetSelected = false;
+
+
+	private float scale_factor= 0.07f;
+	private float MAXSCALE = 6.0f, MIN_SCALE = 0.6f; // zoom-in and zoom-out limits
+	private Vector2 prevDist = new Vector2(0,0);
+	private Vector2 curDist = new Vector2(0,0);
+	private Vector2 midPoint = new Vector2(0,0);
+
+
+	TouchPhase lastTouchPhase;
+
+	Color _lastTint;
+	void rotationEnabled(){
+
+		if (Input.touchCount == 1) {
+			Touch touch = Input.GetTouch (0);
+			switch (touch.phase) {
+
+			case TouchPhase.Began:
+
+				lastTouchPhase = TouchPhase.Began;
+
+				selectObject (touch);
+				return;
+			case TouchPhase.Moved:
+
+				lastTouchPhase = TouchPhase.Moved;
+				rotateAcrossYusingX (touch);
+				return;
+			case TouchPhase.Ended:
+				if (lastTouchPhase == TouchPhase.Moved) {
+					deselectGameObject ();
+				}
+
+				lastTouchPhase = TouchPhase.Moved;
+				return;
+			}
+
+
+
+		}
+	}
+
+	void rotateAcrossYusingX(Touch touch){
+	
+		Vector3 deltaShift = touch.deltaPosition;
+		Vector3 translationVector = new Vector3 (0, -deltaShift.x * PAN_SPEED * 3, 0);
+		selectedGameObject.transform.parent.transform.Rotate(translationVector,Space.Self);
+	}
+		
+	// Update is called once per frame
+	void Update () {
+
+		int touchCount = Input.touchCount; 
+		Touch[] touches = Input.touches;
+		Touch touchFirst = touches [0];
+
+
+		if (rotationWidgetSelected) {
+			rotationEnabled ();
+			return;
+		}
+
+		if (touchCount == 1) {
+		
+			switch (touchFirst.phase) {
+
+			case TouchPhase.Began:
+				
+				lastTouchPhase = TouchPhase.Began;
+
+				selectObject (touchFirst);
+				return;
+			case TouchPhase.Moved:
+				
+				lastTouchPhase = TouchPhase.Moved;
+				panAcross (touchFirst);
+				//panAcross();
+				return;
+
+			case TouchPhase.Ended:
+				if (lastTouchPhase == TouchPhase.Moved) {
+					deselectGameObject ();
+				}
+			
+				lastTouchPhase = TouchPhase.Moved;
+				return;
+			}
+		
+		} else if (touchCount == 2) {
+			
+			switch (touchFirst.phase) {
+
+			case TouchPhase.Moved:
+				if (selectedGameObject == null || touches[1].phase != TouchPhase.Moved) {
+					return;
+				}
+				//zoomObject (touches);
+				scaleAddendum();
+
+				return;
+			}
+				
+		}
+		else if(touchCount == 3){
+
+
+		}
+
+
+	}
+	void selectObject(Touch touch){
+
+		RaycastHit hitObject;
+		Ray ray = Camera.main.ScreenPointToRay (touch.position);
+		if (Physics.Raycast (ray, out hitObject)) {
+			if (hitObject.collider.tag.Equals ("target")) {
+						//switch object script
+				rotationWidgetSelected = false;
+				switchSelectGameObject(hitObject.collider.gameObject);	
+			}else if(hitObject.collider.tag.Equals ("rotation")){
+				rotationWidgetSelected = true;
+				switchSelectGameObject(hitObject.collider.gameObject);	
+			}
+		
+		}
+
+		}
+
+	void switchSelectGameObject(GameObject obj){
+		if (obj == selectedGameObject) {
+		//deselect
+			switchGlow(false,obj);
+			selectedGameObject= null;
+			return;
+		} 
+		switchGlow (false, selectedGameObject);
+		selectedGameObject = obj;
+		switchGlow (true, selectedGameObject);
+	}
+
+	void deselectGameObject(){
+		switchGlow (false, selectedGameObject);
+		selectedGameObject = null;
+	
+	}
+
+	void panAcross(Touch touch){
+		if (selectedGameObject == null) {
+			return;
+		}
+			
+		Vector2 deltaShift = touch.deltaPosition;
+		Vector3 translationVector = new Vector3 (deltaShift.x * PAN_SPEED * Time.deltaTime, deltaShift.y * PAN_SPEED * Time.deltaTime, 0);
+		selectedGameObject.transform.Translate (translationVector,Space.World);
+	}
+		
+
+	void switchGlow(bool flip,GameObject obj){
+		if (obj == null || obj.tag.Equals("rotation")) {
+			return;
+		}
+
+
+		if (!flip) {
+
+			obj.GetComponent<GLOW>().glowOFF();
+		
+		} else {
+
+			obj.GetComponent<GLOW>().glowON();
+		}
+
+	}
+		
+	private void scaleFromPosition(Vector3 scale, Vector3 fromPos)
+	{	
+		selectedGameObject.transform.localScale = scale;
+	
+	}
+
+	private void scaleAddendum(){
+
+		midPoint = new Vector2(((Input.GetTouch(0).position.x + Input.GetTouch(1).position.x)/2), ((Input.GetTouch(0).position.y + Input.GetTouch(1).position.y)/2));
+		midPoint = Camera.main.ScreenToWorldPoint(midPoint);
+		curDist = Input.GetTouch(0).position - Input.GetTouch(1).position; //current distance between finger touches
+		prevDist = ((Input.GetTouch(0).position - Input.GetTouch(0).deltaPosition) - (Input.GetTouch(1).position - Input.GetTouch(1).deltaPosition)); //difference in previous locations using delta positions
+		float touchDelta = curDist.magnitude - prevDist.magnitude;
+		if(touchDelta>0)
+		{
+			if(selectedGameObject.transform.localScale.x < MAXSCALE && selectedGameObject.transform.localScale.y < MAXSCALE && selectedGameObject.transform.localScale.z < MAXSCALE)
+			{
+				Vector3 scale = new Vector3(selectedGameObject.transform.localScale.x + scale_factor, selectedGameObject.transform.localScale.y + scale_factor, selectedGameObject.transform.localScale.z + scale_factor);
+				scale.x = (scale.x > MAXSCALE) ? MAXSCALE : scale.x;
+				scale.y = (scale.y > MAXSCALE) ? MAXSCALE : scale.y;
+				scale.z = (scale.z > MAXSCALE) ? MAXSCALE : scale.z;
+				scaleFromPosition(scale,midPoint);
+			}
+		}
+		//Zoom in
+		else if(touchDelta<0)
+		{
+			if(selectedGameObject.transform.localScale.x > MIN_SCALE && selectedGameObject.transform.localScale.y > MIN_SCALE && selectedGameObject.transform.localScale.z > MIN_SCALE)
+			{
+				Vector3 scale = new Vector3(selectedGameObject.transform.localScale.x + scale_factor*-1, selectedGameObject.transform.localScale.y + scale_factor*-1, selectedGameObject.transform.localScale.z + scale_factor*-1);
+				scale.x = (scale.x < MIN_SCALE) ? MIN_SCALE : scale.x;
+				scale.y = (scale.y < MIN_SCALE) ? MIN_SCALE : scale.y;
+				scale.z = (scale.z < MIN_SCALE) ? MIN_SCALE : scale.z;
+				scaleFromPosition(scale,midPoint);
+			}
+		}
+
+	}
+}
+
+
+
